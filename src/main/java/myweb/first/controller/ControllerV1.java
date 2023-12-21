f82e8d6030386856dd81fdc36987961cd9f38579
jung <mg1997@naver.com>
2023-12-21 10:59:14 +0900

    2023-12-21 commit

--- src/main/java/myweb/first/controller/ControllerV1.java
+++ src/main/java/myweb/first/controller/ControllerV1.java
@@ -1,13 +1,22 @@
 package myweb.first.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 public class ControllerV1 {
     @RequestMapping("/index")
-    String newForm(){
-        System.out.println("ControllerV1.newForm");
+    String newForm(@RequestParam(defaultValue = "1") String n){
+        if(n.equals("1")){
+            log.info("정상 입니다 ={}",n);
+        }else if(n.equals("2")){
+            log.error("에러 입니다 ={}",n);
+        }else{
+            log.warn("경고입니다 ={}",n);
+        }
         return "index";
     }
 }
